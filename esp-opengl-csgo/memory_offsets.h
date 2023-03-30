cbbf86298b81175571995d2d4c2a50ebe8121e07
ZABBIX <zabbix@ztrunk.space>
2023-03-30 20:43:57 +0200

    Update memory_offsets.h

--- esp-opengl-csgo/memory_offsets.h
+++ esp-opengl-csgo/memory_offsets.h
@@ -6,8 +6,8 @@ thanks frk1 for: https://github.com/frk1/hazedumper/blob/master/csgo.hpp
 namespace offsets {
 	/* addreses */
 	uintptr_t addr_local_player = 0xDEA964;
-	uintptr_t addr_entity_lit = 0x4DFFF14;
-	uintptr_t addr_view_matrix = 0x4DF0D44;
+	uintptr_t addr_entity_lit = 0x4E01024;
+	uintptr_t addr_view_matrix = 0x4DF1E54;
 
 	/* Net vars */
 	uintptr_t net_health = 0x100;
